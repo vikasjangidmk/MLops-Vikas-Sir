0b3099c86e30949278cec5922410ba3266a27d55
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-05-31 15:05:50 +0530

    updated

---
+++ OOPS Python/oops_proj.py
@@ -0,0 +1,29 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+        
+    def menu(self):
+        user_input = input("""Welcome to Chatbook! How would you like to proceed? 
+                           1. Press 1 to sign up
+                           2. Press 2 to sign in
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit
+                           """)
+        
+        if user_input == '1':
+            pass
+        elif user_input == '2':
+            pass
+        elif user_input == '3':
+            pass
+        elif user_input == '4':
+            pass
+        else:
+            exit()
+            
+            
+obj = chatbook()

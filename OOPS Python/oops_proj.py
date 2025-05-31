f3e1af59a822e4e8a95f8ed830bc779446c61944
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-05-31 15:24:41 +0530

    updated

--- OOPS Python/oops_proj.py
+++ OOPS Python/oops_proj.py
@@ -15,9 +15,9 @@ class chatbook:
                            """)
         
         if user_input == '1':
-            pass
+            self.signup()
         elif user_input == '2':
-            pass
+            self.signin()
         elif user_input == '3':
             pass
         elif user_input == '4':
@@ -25,5 +25,32 @@ class chatbook:
         else:
             exit()
             
+    def signup(self):
+        email = input("Enter your email here ->: ")
+        pwd = input("setup your password here ->: ")
+    
+        self.username = email
+        self.password = pwd
+        print("You have signed up successfully!")
+        print("\n")
+        self.menu()
+        
+        
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please sign up first!")
+        else:
+            uname = input("Enter your username here ->: ")
+            pwd = input("Enter your password here ->: ")
             
+            if self.username == uname and self.password == pwd:
+               print("You have signed in successfully!")    
+               self.loggedin = True 
+            else:
+                print("Invalid username or password. Please try again.")
+        
+        print("\n")
+        self.menu()
+        
 obj = chatbook()
+

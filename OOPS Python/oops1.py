f260591394a35dbbd70519f1eb39eb97104fff2f
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-05-31 12:50:36 +0530

    updated

--- OOPS Python/oops1.py
+++ OOPS Python/oops1.py
@@ -2,11 +2,14 @@
 class employee:
     # speacial method/ magic method/ dunder method - constructor
     def __init__(self):
+        #print("Started executing the attributes/data")
         self.id = 123
         self.salary = 50000 
         self.designation = "SDE"
+        #print("attributes/data initialized successfully")
         
     def travel(self, destination):
+        print("This travel functio was called manually")
         print(f"Employee is now travelling to {destination}.")
         
         
@@ -17,4 +20,8 @@ sam = employee()
 #print(sam.salary)  # accessing the attribute of the class
 #print(sam.id) # accessing the attribute of the class
 #print(sam.designation)  # accessing the attribute of the class
-sam.travel("Rajsthan") # calling the method of the class
+
+# calling the method of the class
+#sam.travel("Rajsthan") 
+
+print(type(sam))  # checking the type of the object
---
+++ OOPS Python/rough.py
@@ -0,0 +1,7 @@
+lst = [1,2,3]
+my_str = "Vikas jangid"
+my_int = 8
+
+print(type(lst))  # <class 'list'>
+print(type(my_str))  # <class 'str'>    
+print(type(my_int))  # <class 'int'>
